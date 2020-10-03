6166d3260df06214a8e9bb9d0a316c332852ba99
receptor-mammography <64310124+receptor-mammography@users.noreply.github.com>
2020-10-03 10:31:00 +0900

    Update predict.py

--- code/predict.py
+++ code/predict.py
@@ -1,5 +1,6 @@
 #-*- coding: utf-8 -*-
-# 使用するGPUを指定するには環境変数 CUDA_VISIBLE_DEVICES に0-3の値を指定して実行すること。
+
+# Libraries
 import keras
 import tensorflow
 from keras.applications.vgg16 import VGG16
@@ -21,12 +22,11 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import StratifiedKFold, KFold
 
-# パラメータ
 
-# True なら generator を使う。Falseならgeneratorを使わずdatasetをそのまま渡す
+# If "True", predict with generator. If "False", predict without generator.
 use_generator = True
 
-# 重みファイル名(クロスバリデーションなし)
+# Weight path
 model_save_name = '../h5files/ER_VGG_best_performing.hdf5'
 csvlog_name = '../log/SGD.log'
 img_dir_test = '/path/to/test-img-dir'
@@ -35,11 +35,11 @@ classes = ["Negative", "Positive"]
 file_ext = 'png'
 image_size=300
 
-# ----
-
+# Image size
 img_height=image_size
 img_width=image_size
 
+# The number of classes
 num_classes = len(classes)
 
 def load_dataset(path, classes):
@@ -50,7 +50,7 @@ def load_dataset(path, classes):
   for index, ClassLabel in enumerate(classes):
     global use_generator
     ImagesDir = os.path.join(path, ClassLabel)
-    # RGB変換
+    # convert RGB
     print(index)
     print(ClassLabel)
     print(ImagesDir)
@@ -60,16 +60,16 @@ def load_dataset(path, classes):
         print(file)
         image = Image.open(file)
         image = image.convert('RGB')
-        # リサイズ
+        # Resize
         image = image.resize((image_size, image_size))
-        # 画像から配列に変換 
+        # Image to array
         data = np.asarray(image)
         X.append(data)
         Y.append(index)
         Files.append(file)
   X = np.array(X)
   Y = np.array(Y)
-  # データ型の変換＆正規化
+  # Convert data type and normalization
   if not use_generator:
     X = X.astype('float32') / 255
   return (X, Y, np.array(Files))
@@ -80,13 +80,13 @@ print(Xtest.shape, Ytest.shape)
 
 dir_tf_log = '../tf_log'
 
-# セッションの初期化
+# Initialization
 K.clear_session()
 config = tensorflow.ConfigProto(gpu_options=tensorflow.GPUOptions(allow_growth=True))
 session = tensorflow.Session(config=config)
 K.tensorflow_backend.set_session(session)
 
-#訓練データ拡張
+# Augmentation
 horizontal_flip = True
 vertical_flip = True
 datagen = ImageDataGenerator(
@@ -107,10 +107,10 @@ datagen = ImageDataGenerator(
 
 def create_model():
   global image_size
-  #VGGモデルの読み込み。最終層は読み込まない
   input_shape = (image_size, image_size, 3)
+  # Feature extractor
   base_model = VGG16(weights= 'imagenet', include_top=False, input_shape=input_shape)
-  #最終層の設定
+  # Classifier
   m = Sequential()
   m.add(Flatten(input_shape=base_model.output_shape[1:]))
   m.add(Dense(256, activation='relu', kernel_initializer='he_normal'))
@@ -127,20 +127,18 @@ def create_model():
   #   layer.trainable = True
   # model.summary()
 
-  # 学習率
-  #opt = SGD()
+  # Optimizer
   opt = SGD(lr=0.001)
   #opt = rmsprop(lr=5e-7, decay=5e-5)
   model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
-  #model.compile(optimizer=opt, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
   return model
 
 model = create_model()
 model.summary()
 model.load_weights(model_save_name)
 
-# 学習済みモデルを使って予測を行う。
-# x: 予測対象のリスト
+# Prediction
+# x: lists to be predicted
 def predict(x, use_argmax=True):
     global model
 
@@ -157,7 +155,7 @@ print("Predict:")
 for i in np.arange(num_classes):
     x = Xtest[Ytest == i]
     files = testFiles[Ytest == i]
-    # generator で正規化している場合、データ自体が正規化されてないので、先に正規化する
+    # If use generator, normalization is needed. 
     if use_generator:
        x = x / 255.
     
