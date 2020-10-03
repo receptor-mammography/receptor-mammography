7a7b44efd70c9e7590b68cc3c47c26e5e35cd72c
Anonymous <>
2020-10-03 15:56:32 +0900

    removed unnecesarry codes

--- code/train.py
+++ code/train.py
@@ -4,12 +4,6 @@
 import keras
 import tensorflow
 
-# Switch the model which you wanted to use
-from keras.applications.vgg16 import VGG16
-#from keras.applications.inception_v3 import InceptionV3
-#from keras.applications.resnet50 import ResNet50
-#from keras.applications.densenet import DenseNet121
-
 from keras.models import Model, Sequential
 from keras.layers import GlobalAveragePooling2D, Dense, Dropout, Flatten, Input, BatchNormalization
 
@@ -28,9 +22,6 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import StratifiedKFold, KFold
-
-# If "True", you can predict without training algorithm. 
-predict_only = True 
 
 # If "True", you can use generator.
 use_generator = True
@@ -58,7 +49,16 @@ def create_model():
   global image_size
   input_shape = (image_size, image_size, 3)
   # Choose the model from "VGG16, InceptionV3, ResNet50, DenseNet121"
+  from keras.applications.vgg16 import VGG16
   base_model = VGG16(weights= 'imagenet', include_top=False, input_shape=input_shape)
+  # from keras.applications.inception_v3 import InceptionV3
+  # base_model = InceptionV3(weights= 'imagenet', include_top=False, input_shape=input_shape)
+  #
+  # from keras.applications.resnet50 import ResNet50
+  # base_model = ResNet50(weights= 'imagenet', include_top=False, input_shape=input_shape)
+  #
+  # from keras.applications.densenet import DenseNet121
+  # base_model = DenseNet121(weights= 'imagenet', include_top=False, input_shape=input_shape)
   m = Sequential()
   m.add(Flatten(input_shape=base_model.output_shape[1:]))
   m.add(Dense(256, activation='relu', kernel_initializer='he_normal'))
@@ -75,6 +75,7 @@ def create_model():
 # Output directory
 model_save_name = '../h5files/SGD_ensemble_%d.hdf5'
 csvlog_name = '../log/SGD.log'
+dir_tf_log = '../tf_log'
 
 # Definition of downsampling
 def down_sample(x,y, shuffle=True):
@@ -151,7 +152,6 @@ Xtest, Ytest, testFiles = load_dataset(img_dir_test, classes)
 
 print(X.shape, Y.shape)
 
-dir_tf_log = '../tf_log'
 
 # Initialization
 config = tensorflow.ConfigProto(gpu_options=tensorflow.GPUOptions(allow_growth=True))
@@ -189,100 +189,56 @@ script_path = os.path.abspath(__file__)
 skf = KFold(n_splits=n_splits, shuffle=True)
 index = 0
 test_pred = []
-if not predict_only:
-	train_val = []
-	if use_cross_validation:
-		train_val = skf.split(X, Y)
-	else:
-		i = np.random.permutation(np.arange(len(X)))
-		s = int(len(i) * 3 / 4) + 1
-		train_val = [(i[0:s], i[s:])]
-	for train, val in train_val:
-		model = create_model()
-		if index == 0: model.summary()
-		model_name = model_save_name % index
-		index += 1
-		checkpointer = ModelCheckpoint(filepath=model_name, monitor='val_loss', verbose=1, save_best_only=True)
-		for i2 in np.arange(sub_split):
-			print("begin split ",index,'/',n_splits,", subsplit ",i2+1,'/',sub_split)
-			i = down_sample(X[train], Y[train])
-			x, y = X[train][i], Y[train][i]
+train_val = []
+if use_cross_validation:
+	train_val = skf.split(X, Y)
+else:
+	i = np.random.permutation(np.arange(len(X)))
+	s = int(len(i) * 3 / 4) + 1
+	train_val = [(i[0:s], i[s:])]
+for train, val in train_val:
+	model = create_model()
+	if index == 0: model.summary()
+	model_name = model_save_name % index
+	index += 1
+	checkpointer = ModelCheckpoint(filepath=model_name, monitor='val_loss', verbose=1, save_best_only=True)
+	for i2 in np.arange(sub_split):
+		print("begin split ",index,'/',n_splits,", subsplit ",i2+1,'/',sub_split)
+		i = down_sample(X[train], Y[train])
+		x, y = X[train][i], Y[train][i]
 
-			y = [to_categorical(i, num_classes=num_classes) for i in y]
-			
-			i = down_sample(X[val], Y[val])
-			xval, yval = X[val][i], Y[val][i]
-			yval = [to_categorical(i, num_classes=num_classes) for i in yval]
-			
-			i = down_sample(Xtest, Ytest)
-			xtest, ytest = Xtest[i], Ytest[i]
-			ytest = [to_categorical(i, num_classes=num_classes) for i in ytest]
-			x = np.array(x);       y = np.array(y)
-			xval = np.array(xval); yval = np.array(yval)
-			xtest = np.array(xtest); ytest = np.array(ytest)
-			if use_generator:
-				history = model.fit_generator(
-					    datagen.flow(x, y, batch_size = batch_size),
-	        		            steps_per_epoch=x.shape[0] // batch_size,
-	        		            epochs=epochs,
-	        		            validation_data = datagen.flow(xval, yval, batch_size = batch_size),
-	        		            validation_steps=xval.shape[0] // batch_size,
-	        		            verbose=1,
-	        		            callbacks=[csv_logger, checkpointer])
-			else:
-				history = model.fit(x=x, y=y, batch_size=batch_size,
-					epochs=epochs, verbose=1,
-					validation_data = (xval, yval),
-					callbacks=[csv_logger, checkpointer])
-		# Evaluation
+		y = [to_categorical(i, num_classes=num_classes) for i in y]
+		
+		i = down_sample(X[val], Y[val])
+		xval, yval = X[val][i], Y[val][i]
+		yval = [to_categorical(i, num_classes=num_classes) for i in yval]
+		
+		i = down_sample(Xtest, Ytest)
+		xtest, ytest = Xtest[i], Ytest[i]
+		ytest = [to_categorical(i, num_classes=num_classes) for i in ytest]
+		x = np.array(x);       y = np.array(y)
+		xval = np.array(xval); yval = np.array(yval)
+		xtest = np.array(xtest); ytest = np.array(ytest)
 		if use_generator:
-			scores = model.evaluate_generator(datagen.flow(xtest, ytest, batch_size=batch_size), verbose=0)
+			history = model.fit_generator(
+				    datagen.flow(x, y, batch_size = batch_size),
+        		            steps_per_epoch=x.shape[0] // batch_size,
+        		            epochs=epochs,
+        		            validation_data = datagen.flow(xval, yval, batch_size = batch_size),
+        		            validation_steps=xval.shape[0] // batch_size,
+        		            verbose=1,
+        		            callbacks=[csv_logger, checkpointer])
 		else:
-			scores = model.evaluate(x=xtest, y=ytest, batch_size=batch_size, verbose=0)
-		print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
-		test_pred.append(scores[1])
-		K.clear_session()
-
-preds=[]
-models=[]
-
-# Prediction
-# x: list for prediction
-# If "raw=True", the categorical cross-entropy prediction value for each image are provided. If "raw=False", present summary only.
-# model_range: Choice which h5file you use.
-def predict(x, raw=False, model_range=None):
-    global model_save_name, n_splits, preds, use_cross_validation, predict_only
-    pred = []
-    default_model_range = np.arange(n_splits if use_cross_validation else 1)
-    if len(models) == 0:
-      for i in default_model_range:
-          model = create_model()
-          model.load_weights(model_save_name % i)
-          models.append(model)
-      if predict_only:
-          models[0].summary()
-
-    model_range = np.array(model_range) if model_range else default_model_range
-    for i in model_range:
-        model = models[i]
-        p = model.predict(x)
-        pred.append(p)
-    if not raw:
-      pred = np.array(pred)
-      pred = np.sum(pred, axis=0)
-    preds.append(pred)
-    return np.array(pred)
-
-print("Predict:")
-for i in np.arange(num_classes):
-    x = Xtest[Ytest == i]
-    files = testFiles[Ytest == i]
-    if use_generator:
-       x = x / 255.
-    
-    result = predict(x)
-    #result = predict(x, model_range=[1])
-    for k,file in enumerate(files):
-       print("class ", i,"\tpredict ", np.argmax(result[k]), "\t", file, "\t", result[k]) 
-#K.clear_session()
+			history = model.fit(x=x, y=y, batch_size=batch_size,
+				epochs=epochs, verbose=1,
+				validation_data = (xval, yval),
+				callbacks=[csv_logger, checkpointer])
+	# Evaluation
+	if use_generator:
+		scores = model.evaluate_generator(datagen.flow(xtest, ytest, batch_size=batch_size), verbose=0)
+	else:
+		scores = model.evaluate(x=xtest, y=ytest, batch_size=batch_size, verbose=0)
+	print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+	test_pred.append(scores[1])
+	K.clear_session()
 
